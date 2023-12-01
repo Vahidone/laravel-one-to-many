cce0bacdb848e049812ea4fdce1283aa661599fc
Vahidone <v.ban.roma@gmail.com>
2023-12-01 18:58:55 +0100

    feat: make seeder for projects with random type_id

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Type;
 use App\Models\Project;
 use App\Functions\Helper;
 
@@ -19,6 +20,8 @@ class ProjectsTableSeeder extends Seeder
     {
         for($i = 0; $i < 100; $i++){
             $new_project = new Project();
+            // associo randomicamente una type al project con il seguente metodo
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->title = $faker->sentence();
             $new_project->slug = Helper::generateSlug($new_project->title, Project::class);
             $new_project->description = $faker->paragraph();

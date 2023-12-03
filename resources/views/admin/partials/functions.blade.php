da065fa151ec767df31e197b81b331e293e099b1
Vahidone <v.ban.roma@gmail.com>
2023-12-03 15:15:38 +0100

    feat: add a function to remove is-invalid class

--- resources/views/admin/partials/functions.blade.php
+++ resources/views/admin/partials/functions.blade.php
@@ -28,4 +28,28 @@
 
 
 
+
+    // funzione per far scomparire la classe is-invalid
+
+    function hideErrorMessages() {
+    setTimeout(() => {
+        const errorDivs = document.querySelectorAll('.is-invalid');
+
+        // Verifico se ci sono elementi con la classe "is-invalid"
+        if (errorDivs.length > 0) {
+            // faccio un ciclo su tutti gli elementi con la classe e rimuovo la classe "is-invalid"
+            for (const errorDiv of errorDivs) {
+                errorDiv.classList.remove('is-invalid');
+
+            }
+        }
+    }, 3000);
+}
+
+    // Chiamo la funzione
+    hideErrorMessages();
+
+
+
+
 </script>
